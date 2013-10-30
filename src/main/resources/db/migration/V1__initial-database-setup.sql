635fa414e8d465a4f25b347a6ff7135200be1530
Stian Sandve <stian@sandve.org>
2013-10-30 11:00:30 +0100

    Flyway is now up and running

--- src/main/resources/db/migration/V1__initial-database-setup.sql
+++ src/main/resources/db/migration/V1__initial-database-setup.sql
@@ -2,12 +2,6 @@
 -- Put in the create table statements here...
 --
 
-delimiter $$
-
-CREATE DATABASE `PictureBrowser` /*!40100 DEFAULT CHARACTER SET latin1 */$$
-
-USE `PictureBrowser`;$$
-
 CREATE TABLE `images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(64) DEFAULT NULL,
@@ -15,17 +9,17 @@ CREATE TABLE `images` (
   `external_id` varchar(64) DEFAULT NULL,
   `description` text,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `configs` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) DEFAULT NULL,
   `value` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `keywords` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `keyword` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1$$
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
