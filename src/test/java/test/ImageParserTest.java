fd27473298431cbf5887af0ffbed6453104bc4fc
Stian Sandve <stian@sandve.org>
2013-11-03 22:08:59 +0100

    Tests now use SimpleDateFormat to compensate for differences in timezones

--- src/test/java/test/ImageParserTest.java
+++ src/test/java/test/ImageParserTest.java
@@ -2,7 +2,9 @@ package test;
 
 import static org.junit.Assert.*;
 import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import org.junit.Before;
 import org.junit.Test;
 import bll.utils.ImageParser;
 import dal.admin.Image;
@@ -16,23 +18,45 @@ import dal.admin.Image;
 
 public class ImageParserTest {
 	
+	String format;
+	SimpleDateFormat sdf;
+	
+	Date sqlDate;
+	String dateTime;
+	
+	Date sqlDate1;
+	String dateTime1;
+	
+	Date sqlDate2;
+	String dateTime2;
+	
+	@Before
+	public void init() {
+		format = "yyyy-MM-dd HH:mm:ss";
+		sdf = new SimpleDateFormat(format);
+		
+		sqlDate = new Date(0);
+		dateTime = sdf.format(sqlDate);
+		
+		sqlDate1 = new Date(0);
+		dateTime1 = sdf.format(sqlDate1);
+		
+		sqlDate2 = new Date(1000);
+		dateTime2 = sdf.format(sqlDate2);
+	}
+	
 	@Test
 	public void givenImageCeckThatReturnedJsonIsValid() {
-		Date sqlDate = new Date(0);
 		Image img = new Image("url", 1, "description", sqlDate);
 		String json = ImageParser.getJsonFromImage(img);
-		//has to be 1970-01-01 01:00:00 because the program will detect the time zone
-		//that our jvm is set to (which is GMT+1)
 		assertEquals(json, "{\"url\":\"url\",\"id\":1,\"description\":" +
-				"\"description\",\"createdTime\":\"1970-01-01 01:00:00\"}");
+				"\"description\",\"createdTime\":\"" + dateTime + "\"}");
 	}
 	
 	@Test
 	public void givenArrayListOfImagesCeckThatReturnedJsonIsValid() {
-		Date sqlDate1 = new Date(0);
 		Image img1 = new Image("url1", 1, "description1", sqlDate1);
 		
-		Date sqlDate2 = new Date(1000);
 		Image img2 = new Image("url2", 2, "description2", sqlDate2);
 		
 		ArrayList<Image> images = new ArrayList<Image>();
@@ -40,28 +64,26 @@ public class ImageParserTest {
 		images.add(img2);
 		
 		String json = ImageParser.getJsonFromImage(images);
-		//has to be 1970-01-01 01:00:00 because the program will detect the time zone
-		//that our jvm is set to (which is GMT+1)
 		assertEquals(json, "[{\"url\":\"url1\",\"id\":1,\"description\":" +
-				"\"description1\",\"createdTime\":\"1970-01-01 01:00:00\"}" + 
+				"\"description1\",\"createdTime\":\"" + dateTime1 + "\"}" + 
 				",{\"url\":\"url2\",\"id\":2,\"description\":" +
-						"\"description2\",\"createdTime\":\"1970-01-01 01:00:01\"}]");
+						"\"description2\",\"createdTime\":\"" + dateTime2 + "\"}]");
 	}
 	
 	@Test
 	public void givenJsonCheckThatReturnedImagesAreValid() {
 		Date sqlDate1 = new Date(0);
+		String dateTime1 = sdf.format(sqlDate1);
 		Image img1 = new Image("url1", 1, "description1", sqlDate1);
 		
 		Date sqlDate2 = new Date(1000);
+		String dateTime2 = sdf.format(sqlDate2);
 		Image img2 = new Image("url2", 2, "description2", sqlDate2);
 		
-		//has to be 1970-01-01 01:00:00 because the program will detect the time zone
-				//that our jvm is set to (which is GMT+1)
 		String json = "[{\"url\":\"url1\",\"id\":1,\"description\":" +
-				"\"description1\",\"createdTime\":\"1970-01-01 01:00:00\"}" + 
+				"\"description1\",\"createdTime\":\"" + dateTime1 + "\"}" + 
 				",{\"url\":\"url2\",\"id\":2,\"description\":" +
-						"\"description2\",\"createdTime\":\"1970-01-01 01:00:01\"}]";
+						"\"description2\",\"createdTime\":\"" + dateTime2 + "\"}]";
 		
 		ArrayList<Image> images = ImageParser.getImageFromJson(json);
 		Image parsedImg1 = images.get(0);
