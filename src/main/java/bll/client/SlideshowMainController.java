52fdc382e0b24506dd1e79613ab1b3928bb717cb
Stian Sandve <stian@sandve.org>
2013-11-06 15:54:34 +0100

    Fullscreen

--- src/main/java/bll/client/SlideshowMainController.java
+++ src/main/java/bll/client/SlideshowMainController.java
@@ -1,15 +1,11 @@
 package bll.client;
 
-import java.awt.GraphicsDevice;
-import java.awt.GraphicsEnvironment;
-
 import gui.client.MainWindow;
 import javax.swing.JFrame;
 
 public class SlideshowMainController {
 	
-	static GraphicsDevice device = GraphicsEnvironment
-	        .getLocalGraphicsEnvironment().getScreenDevices()[0];
+	
 
 	public static void main(String[] args) {
 		SlideshowController slideshow = new SlideshowController();
@@ -19,6 +15,6 @@ public class SlideshowMainController {
 		wnd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		wnd.pack();
 		wnd.setVisible(true);
-		device.setFullScreenWindow(wnd);
+		wnd.startFullscreen();	
 	}
 }
--- src/main/java/gui/client/MainWindow.java
+++ src/main/java/gui/client/MainWindow.java
@@ -1,11 +1,17 @@
 package gui.client;
 
 import java.awt.BorderLayout;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainWindow extends JFrame {
 	
+	private static GraphicsDevice device = GraphicsEnvironment
+	        .getLocalGraphicsEnvironment().getScreenDevices()[0];
 	private static final long serialVersionUID = -4902720969305740099L;
 
 	public MainWindow(JPanel panel) {
@@ -14,4 +20,35 @@ public class MainWindow extends JFrame {
 		this.add(panel, BorderLayout.CENTER);
 	}
 	
+	public void startFullscreen ()
+	{
+	        if (!device.isFullScreenSupported ())
+	        {
+	            throw new UnsupportedOperationException ("Fullscreen mode is unsupported.");
+	        }
+	        this.addKeyListener(new ExitListener());
+	        try {
+	        	device.setFullScreenWindow (this);
+	        } finally {
+	            device.setFullScreenWindow(null);
+	            System.out.println("could not enable fullscreen exclusive mode");
+	        }
+	}
+	 
+	public void endFullscreenMode ()
+	{
+	        device.setFullScreenWindow (null);
+	        System.exit(0);
+	}
+	
+	private class ExitListener implements KeyListener
+	{
+	        public void keyTyped (KeyEvent e)
+	        {
+	            endFullscreenMode ();
+	        }
+	 
+	        public void keyPressed (KeyEvent e)  {}
+	        public void keyReleased (KeyEvent e) {}
+	}
 }
