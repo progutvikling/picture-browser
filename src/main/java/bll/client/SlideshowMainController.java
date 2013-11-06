0628c6791b0fc86abd77041a9dcd66840639bf70
Stian Sandve <stian@sandve.org>
2013-11-06 11:08:26 +0100

    Full screen slideshow

--- src/main/java/bll/client/SlideshowMainController.java
+++ src/main/java/bll/client/SlideshowMainController.java
@@ -1,9 +1,15 @@
 package bll.client;
 
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
 import gui.client.MainWindow;
 import javax.swing.JFrame;
 
 public class SlideshowMainController {
+	
+	static GraphicsDevice device = GraphicsEnvironment
+	        .getLocalGraphicsEnvironment().getScreenDevices()[0];
 
 	public static void main(String[] args) {
 		SlideshowController slideshow = new SlideshowController();
@@ -13,5 +19,6 @@ public class SlideshowMainController {
 		wnd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		wnd.pack();
 		wnd.setVisible(true);
+		device.setFullScreenWindow(wnd);
 	}
 }
