b5ce43a8af7fa20cc9241ed237eeacb9c2addb01
Stian Sandve <stian@sandve.org>
2013-11-06 16:00:18 +0100

    Bugfix

--- src/main/java/gui/client/MainWindow.java
+++ src/main/java/gui/client/MainWindow.java
@@ -9,9 +9,9 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MainWindow extends JFrame {
-	
+
 	private static GraphicsDevice device = GraphicsEnvironment
-	        .getLocalGraphicsEnvironment().getScreenDevices()[0];
+			.getLocalGraphicsEnvironment().getScreenDevices()[0];
 	private static final long serialVersionUID = -4902720969305740099L;
 
 	public MainWindow(JPanel panel) {
@@ -19,36 +19,39 @@ public class MainWindow extends JFrame {
 		this.setLayout(new BorderLayout());
 		this.add(panel, BorderLayout.CENTER);
 	}
-	
+
 	public void startFullscreen ()
 	{
-	        if (!device.isFullScreenSupported ())
-	        {
-	            throw new UnsupportedOperationException ("Fullscreen mode is unsupported.");
-	        }
-	        this.addKeyListener(new ExitListener());
-	        try {
-	        	device.setFullScreenWindow (this);
-	        } finally {
-	            device.setFullScreenWindow(null);
-	            System.out.println("could not enable fullscreen exclusive mode");
-	        }
+		if (!device.isFullScreenSupported ())
+		{
+			throw new UnsupportedOperationException ("Fullscreen mode is unsupported.");
+		}
+
+		this.addKeyListener(new ExitListener());
+
+		try {
+			device.setFullScreenWindow (this);
+		} 
+		catch (Exception e) {
+			device.setFullScreenWindow(null);
+			System.out.println("could not enable fullscreen exclusive mode");
+		}
 	}
-	 
+
 	public void endFullscreenMode ()
 	{
-	        device.setFullScreenWindow (null);
-	        System.exit(0);
+		device.setFullScreenWindow (null);
+		System.exit(0);
 	}
-	
+
 	private class ExitListener implements KeyListener
 	{
-	        public void keyTyped (KeyEvent e)
-	        {
-	            endFullscreenMode ();
-	        }
-	 
-	        public void keyPressed (KeyEvent e)  {}
-	        public void keyReleased (KeyEvent e) {}
+		public void keyTyped (KeyEvent e)
+		{
+			endFullscreenMode ();
+		}
+
+		public void keyPressed (KeyEvent e)  {}
+		public void keyReleased (KeyEvent e) {}
 	}
 }
