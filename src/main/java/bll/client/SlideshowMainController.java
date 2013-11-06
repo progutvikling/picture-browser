4f41002e84a287c6575bea62cb74c007ac602c29
Stian Sandve <stian@sandve.org>
2013-11-06 19:27:44 +0100

    Fullscreen

--- src/main/java/bll/client/SlideshowMainController.java
+++ src/main/java/bll/client/SlideshowMainController.java
@@ -1,21 +1,36 @@
 package bll.client;
 
+import java.awt.DisplayMode;
+
 import gui.client.MainWindow;
 import javax.swing.JFrame;
 
 public class SlideshowMainController {
 	
+	private static SlideshowController slideshow;
+	private static MainWindow wnd;
 	
-
 	public static void main(String[] args) {
-		SlideshowController slideshow = new SlideshowController();
-		
-		MainWindow wnd = new MainWindow(slideshow.getView());
-		wnd.setUndecorated(true);
-		slideshow.start();
+		initializeSlideshow();
+		setFullScreenMode();
+		startSlideshow();
+	}
+	
+	private static void initializeSlideshow() {
+		slideshow = new SlideshowController();
+		wnd = new MainWindow(slideshow.getView());
 		wnd.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+	
+	private static void setFullScreenMode() {
+		DisplayMode dm=new DisplayMode(800, 600, 16, DisplayMode.REFRESH_RATE_UNKNOWN);
+		wnd.setFullScreenMode(dm);
 		wnd.pack();
 		wnd.setVisible(true);
-		//wnd.startFullscreen();	
+	}
+	
+	private static void startSlideshow() {
+		slideshow.start();
 	}
 }
--- src/main/java/gui/client/MainWindow.java
+++ src/main/java/gui/client/MainWindow.java
@@ -1,58 +1,61 @@
 package gui.client;
 
 import java.awt.BorderLayout;
+import java.awt.DisplayMode;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 
 public class MainWindow extends JFrame {
 
-	private static GraphicsDevice device = GraphicsEnvironment
-			.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+	private static GraphicsDevice device;
+	private JPanel panel;
 	private static final long serialVersionUID = -4902720969305740099L;
 
 	public MainWindow(JPanel panel) {
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		device = ge.getDefaultScreenDevice();
+		this.panel = panel;
 		this.setTitle("Picture Browser");
 		this.setLayout(new BorderLayout());
 		this.add(panel, BorderLayout.CENTER);
-		this.addKeyListener(new ExitListener());
+		this.setKeyBinding();
 	}
-
-	public void startFullscreen ()
-	{
-		if (!device.isFullScreenSupported ())
-		{
-			throw new UnsupportedOperationException ("Fullscreen mode is unsupported.");
+	
+	private void setKeyBinding() {
+		panel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_ESCAPE,0),"escapeDown");
+		panel.getActionMap().put("escapeDown",new AbstractAction() {
+		    private static final long serialVersionUID = 1l;
+		    @Override public void actionPerformed(ActionEvent e) {
+		        endFullScreenMode();
+		    }
+		});
+	}
+	
+	public void setFullScreenMode(DisplayMode dm){
+		this.setUndecorated(true);
+		this.setResizable(false);
+		device.setFullScreenWindow(this);
+		if(device.isDisplayChangeSupported() && dm!=null){
+			try{
+				device.setDisplayMode(dm);
+			}
+			catch(Exception ex){}
 		}
 
-		
-
-		try {
-			device.setFullScreenWindow (this);
-		} 
-		catch (Exception e) {
-			device.setFullScreenWindow(null);
-			System.out.println("could not enable fullscreen exclusive mode");
-		}
 	}
-
-	public void endFullscreenMode ()
-	{
-		device.setFullScreenWindow (null);
-		System.exit(0);
-	}
-
-	private class ExitListener implements KeyListener
-	{
-		public void keyTyped (KeyEvent e)
-		{
-			endFullscreenMode ();
+	
+	public void endFullScreenMode(){
+		Window w = device.getFullScreenWindow();
+		if(w != null){
+			w.dispose();
 		}
-
-		public void keyPressed (KeyEvent e)  {}
-		public void keyReleased (KeyEvent e) {}
+		device.setFullScreenWindow(null);
 	}
 }
