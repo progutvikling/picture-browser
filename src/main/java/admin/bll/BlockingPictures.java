3195fcd8fbe932f94bbe914823cbb6ab15aa17ca
Morten Wærsland <Morten@Mortens-MacBook-Air.local>
2013-11-14 18:13:23 +0100

    Load pictures in background/update Block func.

--- src/main/java/admin/bll/BlockingPictures.java
+++ src/main/java/admin/bll/BlockingPictures.java
@@ -1,6 +1,7 @@
 package admin.bll;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.RenderingHints;
@@ -9,12 +10,16 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.SwingWorker;
+
 import common.dal.IImageStore;
 import common.dal.Image;
 import common.dal.StoreFactory;
@@ -27,70 +32,34 @@ public class BlockingPictures extends JPanel implements MouseListener {
 	 */
 	private static final long serialVersionUID = 6747143681976968595L;
 
-	private JLabel label[];
-	private BufferedImage buffImage;
-	private ImageIcon imgIcon;
-	private List<Image> lImages;
 	private int kol = 4;
 	private IImageStore imagestore;
+	private JPanel picPanel;
+	private loadInBackground loadImage;
+	private int imageLength;
+	private int storeLabel = 0;
+	private JLabel temp[];
+	private List<Image> lImagesFromLoad;
 
 	public BlockingPictures(IImageStore imagestore) {
 		this.setName("Block");
 		this.imagestore = imagestore;
-		ImageLoader();
-		JPanel picPanel = new JPanel();
-		picPanel.setLayout(new GridLayout((lImages.size() / kol) + 1, kol, 5, 5));//Skal være lImage.size()/kol
-		//picPanel.setLayout(new FlowLayout(lImages.size()/kol,5,5));
-		label = new JLabel[lImages.size()];
-		System.out.println("Antall bilder: " + lImages.size());
+		lImagesFromLoad = imagestore.getLast(100);
+		picPanel = new JPanel();
+		loadImage = new loadInBackground(imagestore, this);
+		loadImage.execute();
 
-		int count = 0;
-		//int count2 = 0;
-
-		if (lImages.size() != 0) {
-			if (lImages.size() < 30) {
-				for (int i = 0; i < lImages.size(); i++) {
-					count++;
-					System.out.println(count);
-					buffImage = loadImage(lImages.get(i).getUrl());
-					if(buffImage == null){
-						//count2++;
-						//System.out.println("feilet" + count2);
-						continue;
-					}
-					buffImage = resize(buffImage);
-					if (buffImage != null) {
-						imgIcon = new ImageIcon(buffImage);
-						label[i] = new JLabel(imgIcon);
-						label[i].addMouseListener(createMouseListener());
-						picPanel.add(label[i]);
-					}
-				}
-			}
-
-			// Bare for å slippe å vente på loading av bilde enn så lenge if
-			if (lImages.size() >= 30) {
-				for (int i = 0; i <30; i++) {
-					count++;
-					System.out.println("mer enn 30: " + count);
-					buffImage = loadImage(lImages.get(i).getUrl());
-					if(buffImage == null){
-					//	count2++;
-					//	System.out.println("-------------------------feilet" + count2);
-						continue;
-					}
-					buffImage = resize(buffImage);
-					if (buffImage != null) {
-						imgIcon = new ImageIcon(buffImage);
-						label[i] = new JLabel(imgIcon);
-						label[i].addMouseListener(createMouseListener());
-						picPanel.add(label[i]);
-					}
-				}
-			}
-
-		}
-
+		/**
+		 * Burde ikkje gjør dette sidde eg benytter getLast() også i
+		 * loadInBavkgroun klassen.Dobbelt med bilder. Fant ingen god måte å få
+		 * antall bilder, alltid hentet før klassen var ferdig med å laste
+		 * bilder...
+		 * 
+		 */
+		imageLength = lImagesFromLoad.size();
+		temp = new JLabel[imageLength];
+		
+		
 		JScrollPane scrollpane = new JScrollPane(picPanel);
 		scrollpane
 				.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
@@ -100,59 +69,48 @@ public class BlockingPictures extends JPanel implements MouseListener {
 		this.setLayout(new BorderLayout());
 		this.add(scrollpane, BorderLayout.CENTER);
 		setVisible(true);
-	}
-
-	public void ImageLoader() {
-		lImages=imagestore.getLast(100);
 
 	}
 
-	private BufferedImage loadImage(String urlString) {
-		BufferedImage bImages = null;
-		try {
-			URL url = new URL(urlString);
-			bImages = ImageIO.read(url);
-		} catch (Exception e) {
-			return null;
-		}
-		return bImages;
+	public void setLabel(List<JLabel> l) {
+		setPanel();
+		JLabel imageIconLabel = new JLabel();
+		imageIconLabel = l.get(0);
+		temp[storeLabel] = imageIconLabel;
+		temp[storeLabel].addMouseListener(createMouseListener());
+		picPanel.add(temp[storeLabel]);
+		storeLabel++;
+		System.out.println("storelable: " + storeLabel);
 	}
 
-	public static BufferedImage resize(BufferedImage img) {
-		try {
-			int w = img.getWidth();
-
-			int h = img.getHeight();
-			int newH = 130;
-			int newW = 130;
-			BufferedImage dimg = new BufferedImage(newH, newW, img.getType());
-			Graphics2D g = dimg.createGraphics();
-			g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-					RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-			g.drawImage(img, 0, 0, newW, newH, 0, 0, w, h, null);
-			g.dispose();
-			return dimg;
-		} catch (NullPointerException e) {
-			return null;
-		}
+	public void setPanel() {
+		lImagesFromLoad = loadImage.getImage();
+		// imageSize = lImagesFromLoad.size();
+		// System.out.println(imageSize);
+		picPanel.setLayout(new GridLayout((imageLength / kol) + 1, kol, 5, 5));
 	}
 
 	public MouseAdapter createMouseListener() {
+
 		return new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				JLabel coverLabel = (JLabel) e.getSource();
-				for (int i = 0; i < lImages.size(); i++) {
-					if (e.getSource().equals(label[i])) {
+				for (int i = 0; i < storeLabel; i++) {
+					if (e.getSource().equals(temp[i])) {
 						if (!coverLabel.isEnabled()) {
 							coverLabel.setEnabled(true);
-							storeblocking.unBlock(lImages.get(i).getID());
-							//System.out.println("unblock "+ lImages.get(i).getID());
+							storeblocking.unBlock(lImagesFromLoad.get(i)
+									.getID());
+							System.out.println("unblock "
+									+ lImagesFromLoad.get(i).getID());
 
 						} else {
 							coverLabel.setEnabled(false);
-							storeblocking.block(lImages.get(i).getID());
-							//System.out.println("block "+ lImages.get(i).getID());
+							// System.out.println("----------------------image id: "+lImagesFromLoad.get(i).getID());
+							storeblocking.block(lImagesFromLoad.get(i).getID());
+							System.out.println("block "
+									+ lImagesFromLoad.get(i).getID());
 
 						}
 					}
@@ -193,4 +151,5 @@ public class BlockingPictures extends JPanel implements MouseListener {
 		// TODO Auto-generated method stub
 
 	}
+
 }
---
+++ src/main/java/admin/bll/loadInBackground.java
@@ -0,0 +1,104 @@
+package admin.bll;
+
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.util.List;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.SwingWorker;
+import common.dal.IImageStore;
+import common.dal.Image;
+
+public class loadInBackground extends SwingWorker<JLabel, JLabel> {
+	private JLabel label;
+	private BufferedImage buffImage;
+	private ImageIcon imgIcon;
+	private List<Image> lImages;
+	private IImageStore imagestore;
+	int count2 = 0;
+
+	private BlockingPictures blockPic;
+
+	public loadInBackground(IImageStore imagestore, BlockingPictures bp) {
+		this.imagestore = imagestore;
+		blockPic = bp;
+	}
+
+	protected JLabel doInBackground() throws Exception {
+		ImageLoader();
+		System.out.println("Antall bilder: " + lImages.size());
+		label = new JLabel();
+
+		if (lImages.size() != 0) {
+			for (int i = 0; i < lImages.size(); i++) {
+				buffImage = loadImage(lImages.get(i).getUrl());
+				buffImage = resize(buffImage);
+				if (buffImage == null) {
+					count2++;
+					System.out.println("<--Klarte ikke å resize bilet : "
+							+ count2 + "-->");
+					continue;
+				}
+				if (buffImage != null) {
+					imgIcon = new ImageIcon(buffImage);
+					label = new JLabel(imgIcon);
+					publish(label);
+
+				}
+
+			}
+		}
+		return label;
+	}
+
+	@Override
+	protected void process(List<JLabel> label) {
+		blockPic.setLabel(label);
+	}
+
+	public List<Image> getImage() {
+		return lImages;
+	}
+
+	private BufferedImage loadImage(String urlString) {
+		BufferedImage bImages = null;
+		try {
+			URL url = new URL(urlString);
+			bImages = ImageIO.read(url);
+		} catch (Exception e) {
+			return null;
+		}
+		return bImages;
+	}
+
+	public static BufferedImage resize(BufferedImage img) {
+		try {
+			int w = img.getWidth();
+			int h = img.getHeight();
+			int newH = 130;
+			int newW = 130;
+			BufferedImage dimg = new BufferedImage(newH, newW, img.getType());
+			Graphics2D g = dimg.createGraphics();
+			g.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+					RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+			g.drawImage(img, 0, 0, newW, newH, 0, 0, w, h, null);
+			g.dispose();
+			return dimg;
+		} catch (NullPointerException e) {
+			return null;
+		}
+	}
+
+	public void ImageLoader() {
+		lImages = imagestore.getLast(100);
+
+	}
+	//Denne må vente litt før den bli kallet, det burde funke.
+	public int getImageListSize(){
+		return lImages.size();
+	}
+
+}
