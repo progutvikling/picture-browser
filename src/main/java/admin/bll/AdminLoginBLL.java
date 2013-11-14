feaabbe49837446e8e51cb54be4310ea2f1a2de3
Stian Sandve <stian@sandve.org>
2013-11-14 12:12:26 +0100

    Refactoring

--- src/main/java/admin/bll/AdminLoginBLL.java
+++ src/main/java/admin/bll/AdminLoginBLL.java
@@ -1,8 +1,8 @@
 package admin.bll;
 
 public class AdminLoginBLL {
-	public boolean checkusernameandpassword(String password, String username){
-			if(password.equals("admin") && username.equals("admin")){
+	public boolean checkusernameandpassword(String username, String password){
+			if(username.equals("admin") && password.equals("admin")){
 				return true;
 			}
 		return false;
--- src/main/java/admin/gui/LoginDialog.java
+++ src/main/java/admin/gui/LoginDialog.java
@@ -1,6 +1,5 @@
 package admin.gui;
 
-import java.awt.Color;
 import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -10,109 +9,184 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.Locale;
 import java.util.ResourceBundle;
-
 import javax.swing.*;
-import javax.swing.border.LineBorder;
-
 import admin.bll.AdminLoginBLL;
 
+/**
+ * This class is responsible for authenticating
+ * users that want to log in to the administration
+ * application.
+ */
 
 public class LoginDialog extends JDialog{
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = -535959533872064544L;
-	JPanel frame, panel, buttons, panel2;
-	JButton ok, avbryt;
-	JLabel brukernavn, passord;
-	JTextField brukernavninput;
-	JPasswordField passordinput;
-	private ResourceBundle rb;
 	
+	private JPanel mainPanel, pnlUsername, actions, pnlPassword;
+	private JButton ok, cancel;
+	private JLabel lblUsername, lblPassword;
+	private JTextField username;
+	private JPasswordField password;
+	private ResourceBundle rb;
+	private GridBagConstraints cs;
+	private LoginAction loginAction;
+
 	public LoginDialog(Frame parent){
 		super(parent, true);
-		
-		Locale norwegian = new Locale("no_NO");
-		this.rb = ResourceBundle.getBundle("Strings", norwegian);
-		
+		initialize();	
+		setLocationRelativeTo(parent);
+	}
+
+	private void initialize() {
+		loginAction = new LoginAction();
+		initializeResourceBundles();
+		initializeUI();
+	}
+
+	private void initializeUI() {
 		setTitle(rb.getString("login"));
-		
 		setSize(360, 150);
-        setResizable(false);
-        //Vist man "krysser ut dialogen" s�� avslutter programmet
-        addWindowListener(new WindowAdapter() {
-            @Override public void windowClosing(WindowEvent e) {
-                System.exit(0);
-            }
-        });
+		setResizable(false);
 
-		LoginAction loginAction = new LoginAction();
-
-        //Legger til labels og tekstfields for logininputen.
-		frame = new JPanel();
-		panel = new JPanel(new GridBagLayout());
-		panel2 = new JPanel(new GridBagLayout());
-		GridBagConstraints cs = new GridBagConstraints();
-		cs.fill = GridBagConstraints.HORIZONTAL;
-		brukernavn = new JLabel(rb.getString("username") + ": ");
-		cs.gridx = 0;
-		cs.gridy = 0;
-		cs.gridwidth = 1;
-		panel.add(brukernavn, cs);
-		brukernavninput = new JTextField(20);
-		cs.gridx = 1;
-		cs.gridy = 0;
-		cs.gridwidth = 2;
-		panel.add(brukernavninput, cs);
-		passord = new JLabel(rb.getString("password") + ": ");
-		cs.gridx = 0;
-		cs.gridy = 1;
-		cs.gridwidth = 1;
-		panel2.add(passord, cs);
-		passordinput = new JPasswordField(20);
-		passordinput.addActionListener(loginAction);
-		cs.gridx = 1;
-		cs.gridy = 1;
-		cs.gridwidth = 2;
-		panel2.add(passordinput, cs);
+		terminateWhenClosed();
 		
-		//Lagger knappene og logikken deres.
-		ok = new JButton(rb.getString("login"));
-		//Vist bruker trykker login, sjekker programmet om input var korrekt.
-		ok.addActionListener(loginAction);
-		avbryt = new JButton(rb.getString("cancel"));
-		//Avslutter programmet
-		avbryt.addActionListener(new ActionListener() {
+		initializePanels();
 
+		initializeGridBagConstraints();
+		
+		addUsername();
+		addPassword();
+
+		addOkButton();
+		addCancelButton();
+		
+		addPanelsToMainPanel();
+
+		add(mainPanel);
+	}
+	
+	/**
+	 * Set language to norwegian
+	 */
+	private void initializeResourceBundles() {
+		Locale norwegian = new Locale("no_NO");
+		rb = ResourceBundle.getBundle("Strings", norwegian);
+	}
+	
+	/**
+	 * If the dialog is closed, the application is terminated
+	 */
+	private void terminateWhenClosed() {
+		addWindowListener(new WindowAdapter() {
+			@Override public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+	
+	private void initializePanels() {
+		actions = new JPanel();
+		mainPanel = new JPanel();
+		pnlUsername = new JPanel(new GridBagLayout());
+		pnlPassword = new JPanel(new GridBagLayout());
+	}
+	
+	private void initializeGridBagConstraints() {
+		cs = new GridBagConstraints();
+		cs.fill = GridBagConstraints.HORIZONTAL;
+	}
+	
+	/**
+	 * Initialize and add user name label and 
+	 * text field to the user name panel
+	 */
+	private void addUsername() {
+		lblUsername = new JLabel(rb.getString("username") + ": ");
+		
+		cs.gridx = 0;
+		cs.gridy = 0;
+		cs.gridwidth = 1;
+		
+		pnlUsername.add(lblUsername, cs);
+		
+		username = new JTextField(20);
+		
+		cs.gridx = 1;
+		cs.gridy = 0;
+		cs.gridwidth = 2;
+		
+		pnlUsername.add(username, cs);
+	}
+	
+	/**
+	 * Initialize and add password label and text
+	 * field to the password panel 
+	 */
+	private void addPassword() {
+		lblPassword = new JLabel(rb.getString("password") + ": ");
+		
+		cs.gridx = 0;
+		cs.gridy = 1;
+		cs.gridwidth = 1;
+		
+		pnlPassword.add(lblPassword, cs);
+		
+		password = new JPasswordField(20);
+		password.addActionListener(loginAction);
+		
+		cs.gridx = 1;
+		cs.gridy = 1;
+		cs.gridwidth = 2;
+		
+		pnlPassword.add(password, cs);
+	}
+	
+	/**
+	 * Initialize and add the "Ok" button to
+	 * the actions panel
+	 */
+	private void addOkButton() {
+		ok = new JButton(rb.getString("login"));
+		ok.addActionListener(loginAction);
+		actions.add(ok);
+	}
+	
+	/**
+	 * Initialize and add the "Cancel" button
+	 * to the actions panel
+	 */
+	private void addCancelButton() {
+		cancel = new JButton(rb.getString("cancel"));
+		cancel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.exit(-1);
 			}
 		});
-		
-		//Legger alle panel sammen og legger dei til i dialog vinduet.
-		buttons = new JPanel();
-		buttons.add(ok);
-		buttons.add(avbryt);
-		panel.setBorder(new LineBorder(Color.GRAY));
-		panel2.setBorder(new LineBorder(Color.GRAY));
-		frame.add(panel);
-		frame.add(panel2);
-		frame.add(buttons);
-		add(frame);
-        setLocationRelativeTo(parent);
+
+		actions.add(cancel);
 	}
 
+	private void addPanelsToMainPanel() {
+		mainPanel.add(pnlUsername);
+		mainPanel.add(pnlPassword);
+		mainPanel.add(actions);
+	}
+
+	/**
+	 * Authenticate user name and password.
+	 * Display an error message if authentication fails.
+	 */
 	public class LoginAction implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent evt) {
-			String bruker = brukernavninput.getText();
-			String pass = new String(passordinput.getPassword());
+			String user = username.getText();
+			String pwd = new String(password.getPassword());
 			AdminLoginBLL login = new AdminLoginBLL();
-			if(login.checkusernameandpassword(pass, bruker)){
+			if(login.checkusernameandpassword(user, pwd)){
 				dispose();
 			}
 			else{
-				JOptionPane.showMessageDialog(frame, rb.getString("login_error"));
+				JOptionPane.showMessageDialog(mainPanel, rb.getString("login_error"));
 			}
 		}
 	}
