ff2e90b5994baf76e015f308da0cbf10c6aeffc8
Knut Helland <knut@Knuts-MacBook-Air.local>
2013-11-14 18:49:49 +0100

    Moved blocking-tests over to ImageStoreTest. Closes #25

--- src/test/java/test/common/ImageStoreTest.java
+++ src/test/java/test/common/ImageStoreTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.sql.Date;
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -34,4 +35,45 @@ public class ImageStoreTest {
 		ArrayList<Image> images = store.getLast(10);
 		assertTrue("Should return maximum 10 images", images.size() <= 10);
 	}
+	
+	@Test
+	public void setBlock() {
+		assertTrue("should retrun true if added", store.block(getAValidImageId()));
+	}
+
+	@Test
+	public void removeBlock(){
+		assertTrue("Should return true if removed", store.unBlock(getAValidImageId()));
+	}
+	
+	
+	/**
+	 * Helper function to get a image id
+	 */
+	public static Long getAValidImageId() {
+		Long id = getAId();
+		if (id == null) {
+
+			Image img = new Image("http://dummy.com/img.jpg", (long)(Math.random()*10000), "Description", "keyword", new Date(0001L));
+			IImageStore imageStore = StoreFactory.getImageStore();
+			imageStore.insert(img);
+
+			id = getAId();
+			if (id == null) {
+				throw new RuntimeException("There are no images in DB, and I can't insert one. Cannot run tests without.");
+			}
+		}
+		return id;
+	}
+
+	private static Long getAId() {
+		IImageStore imageStore = StoreFactory.getImageStore();
+		List<Image> images = imageStore.getLast(1);
+		if (images.size() > 0) {
+			return new Long(images.get(0).getID());
+		} else {
+			return null;
+		}
+	}
+	
 }
